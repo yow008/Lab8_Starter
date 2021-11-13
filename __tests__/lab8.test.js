a215198e05c121e461d1aed626763e860617062e
You Wu <89385803+yow008@users.noreply.github.com>
2021-11-12 23:19:33 -0800

    Add files via upload

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -23,15 +23,19 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
+    /*Step 1 ADDED*/
+    for(let i = 0; i < prodItems.length; i++) {
+    console.log(`Checking product item ${i+1}/${prodItems.length}`);
     // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
+    data = await prodItems[i].getProperty('data');
     // Convert that property to JSON
     plainValue = await data.jsonValue();
     // Make sure the title, price, and image are populated in the JSON
     if (plainValue.title.length == 0) { allArePopulated = false; }
     if (plainValue.price.length == 0) { allArePopulated = false; }
     if (plainValue.image.length == 0) { allArePopulated = false; }
+    /*Step 1 ADDED*/
+    }
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
 
@@ -50,6 +54,12 @@ describe('Basic user flow for Website', () => {
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
     // Once you have the button, you can click it and check the innerText property of the button.
     // Once you have the innerText property, use innerText['_remoteObject'].value to get the text value of it
+    const product = await page.$('product-item')
+    const shadowRootA = await product.getProperty('shadowRoot')
+    const buttonA = await shadowRootA.$('button')
+    await buttonA.click()
+    const innerTextProperty = await buttonA.getProperty('innerText')
+    expect(innerTextProperty['_remoteObject'].value).toBe("Remove from Cart")
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
@@ -60,6 +70,15 @@ describe('Basic user flow for Website', () => {
     // Query select all of the <product-item> elements, then for every single product element
     // get the shadowRoot and query select the button inside, and click on it.
     // Check to see if the innerText of #cart-count is 20
+    const prodItems = await page.$$('product-item')
+    for(let i = 1; i < prodItems.length; i++) {
+      const shadowRootE = await prodItems[i].getProperty('shadowRoot')
+      const buttonE = await shadowRootE.$('button')
+      await buttonE.click()
+    }
+    const numOfItemsInCart = await page.$('#cart-count')
+    const innerTextProperty = await numOfItemsInCart.getProperty('innerText')
+    expect(innerTextProperty['_remoteObject'].value).toBe("20")
   }, 10000);
 
   // Check to make sure that after you reload the page it remembers all of the items in your cart
@@ -69,6 +88,17 @@ describe('Basic user flow for Website', () => {
     // Reload the page, then select all of the <product-item> elements, and check every
     // element to make sure that all of their buttons say "Remove from Cart".
     // Also check to make sure that #cart-count is still 20
+    await page.reload()
+    const prodItems = await page.$$('product-item')
+    for(let i = 0; i < prodItems.length; i++) {
+      const shadowRootE = await prodItems[i].getProperty('shadowRoot')
+      const buttonE = await shadowRootE.$('button')
+      const innerTextProperty = await buttonE.getProperty('innerText')
+      expect(innerTextProperty['_remoteObject'].value).toBe("Remove from Cart")
+    }
+    const numOfItemsInCart = await page.$('#cart-count')
+    const innerTextProperty = await numOfItemsInCart.getProperty('innerText')
+    expect(innerTextProperty['_remoteObject'].value).toBe("20")
   }, 10000);
 
   // Check to make sure that the cart in localStorage is what you expect
@@ -76,6 +106,8 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 5
     // At this point he item 'cart' in localStorage should be 
     // '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
+    const cartArray = await page.evaluate(() => {return window.localStorage.getItem('cart')})
+    expect(cartArray).toBe('[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]')
   });
 
   // Checking to make sure that if you remove all of the items from the cart that the cart
@@ -85,6 +117,15 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 6
     // Go through and click "Remove from Cart" on every single <product-item>, just like above.
     // Once you have, check to make sure that #cart-count is now 0
+    const prodItems = await page.$$('product-item')
+    for(let i = 0; i < prodItems.length; i++) {
+      const shadowRootE = await prodItems[i].getProperty('shadowRoot')
+      const buttonE = await shadowRootE.$('button')
+      await buttonE.click()
+    }
+    const numOfItemsInCart = await page.$('#cart-count')
+    const innerTextProperty = await numOfItemsInCart.getProperty('innerText')
+    expect(innerTextProperty['_remoteObject'].value).toBe("0")
   }, 10000);
 
   // Checking to make sure that it remembers us removing everything from the cart
@@ -95,6 +136,17 @@ describe('Basic user flow for Website', () => {
     // Reload the page once more, then go through each <product-item> to make sure that it has remembered nothing
     // is in the cart - do this by checking the text on the buttons so that they should say "Add to Cart".
     // Also check to make sure that #cart-count is still 0
+    await page.reload()
+    const prodItems = await page.$$('product-item')
+    for(let i = 0; i < prodItems.length; i++) {
+      const shadowRootE = await prodItems[i].getProperty('shadowRoot')
+      const buttonE = await shadowRootE.$('button')
+      const buttonText = await buttonE.getProperty('innerText')
+      expect(buttonText['_remoteObject'].value).toBe("Add to Cart")
+    }
+    const numOfItemsInCart = await page.$('#cart-count')
+    const cartText = await numOfItemsInCart.getProperty('innerText')
+    expect(cartText['_remoteObject'].value).toBe("0")
   }, 10000);
 
   // Checking to make sure that localStorage for the cart is as we'd expect for the
@@ -103,5 +155,7 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the localStorage...');
     // TODO - Step 8
     // At this point he item 'cart' in localStorage should be '[]', check to make sure it is
+    const cartArray = await page.evaluate(() => {return window.localStorage.getItem('cart')})
+    expect(cartArray).toBe('[]')
   });
 });
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,83 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Part 2
+test('test a valid phone number', () => {
+    expect(functions.isPhoneNumber('(949) 345-8793')).toBe(true)
+})
+
+test('test a valid phone number', () => {
+    expect(functions.isPhoneNumber('838-948-2948')).toBe(true)
+})
+
+test('test an invalid phone number', () => {
+    expect(functions.isPhoneNumber('19898098202')).toBe(false)
+})
+
+test('test an invalid phone number', () => {
+    expect(functions.isPhoneNumber('103-idp-3928')).toBe(false)
+})
+
+test('test a valid email', () => {
+    expect(functions.isEmail('isusoe@qi.com')).toBe(true)
+})
+
+test('test a valid email', () => {
+    expect(functions.isEmail('20392039@Qart.cN')).toBe(true)
+})
+
+test('test an invalid email', () => {
+    expect(functions.isEmail('isusoe@qi.c')).toBe(false)
+})
+
+test('test an invalid email', () => {
+    expect(functions.isEmail('20392039@QartcN')).toBe(false)
+})
+
+test('test a valid/strong password', () => {
+    expect(functions.isStrongPassword('Ad28')).toBe(true)
+})
+
+test('test a valid/strong password', () => {
+    expect(functions.isStrongPassword('z2I_8birth00__e')).toBe(true)
+})
+
+test('test a(n) invalid/weak password', () => {
+    expect(functions.isStrongPassword('z2i8ieicsecie_lol')).toBe(false)
+})
+
+test('test a(n) invalid/weak password', () => {
+    expect(functions.isStrongPassword('96iii')).toBe(false)
+})
+
+test('test a valid date', () => {
+    expect(functions.isDate('09/10/2019')).toBe(true)
+})
+
+test('test a valid date', () => {
+    expect(functions.isDate('19/1/2020')).toBe(true)
+})
+
+test('test an invalid date', () => {
+    expect(functions.isDate('9/9/19')).toBe(false)
+})
+
+test('test an invalid date', () => {
+    expect(functions.isDate('2020.11.11')).toBe(false)
+})
+
+test('test a valid hex code for a color', () => {
+    expect(functions.isHexColor('#e93Dd6')).toBe(true)
+})
+
+test('test a valid hex code for a color', () => {
+    expect(functions.isHexColor('449')).toBe(true)
+})
+
+test('test a valid hex code for a color', () => {
+    expect(functions.isHexColor('#4830')).toBe(false)
+})
+
+test('test a valid hex code for a color', () => {
+    expect(functions.isHexColor('#10e43p')).toBe(false)
+})
